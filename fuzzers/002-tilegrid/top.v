cb822ce935c2ff61c4e6cdbb4e8c9881a2d80875
Wojciech Tatarski <wtatarski@antmicro.com>
2021-06-04 14:13:03 +0200

    Add missing license headers
    Signed-off-by: Wojciech Tatarski <wtatarski@antmicro.com>

--- fuzzers/002-tilegrid/top.v
+++ fuzzers/002-tilegrid/top.v
@@ -1,3 +1,11 @@
+// Copyright (C) 2020-2021  The Project U-Ray Authors.
+//
+// Use of this source code is governed by a ISC-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/ISC
+//
+// SPDX-License-Identifier: ISC
+
 module top(input clk, stb, di, output do);
 	roi roi(
 		.clk(clk),
--- utils/top_generate.mk
+++ utils/top_generate.mk
@@ -1,3 +1,11 @@
+# Copyright (C) 2020-2021  The Project U-Ray Authors.
+#
+# Use of this source code is governed by a ISC-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/ISC
+#
+# SPDX-License-Identifier: ISC
+
 all: OK
 
 OK: generate.ok
